c8e09cf17c3c435228a2ce1174c4317894e799a8
Samuel <samucabalena@gmail.com>
2022-05-31 22:08:38 -0300

    chore: add babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path') // Importando o path para mexer no diretório
+
+module.exports = {
+    entry: path.resolve(__dirname,'src', 'index.jsx'), // configurando os arquivos de entrada
+    output: {
+        path: path.resolve(__dirname,'dist'), // configurando a pasta que vai sair
+        filename: 'bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                
+            }
+        ]
+    }
+}
