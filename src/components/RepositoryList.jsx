612c74a5a90242f8e8235de25fdd53bf9187032d
Samuel <samucabalena@gmail.com>
2022-06-10 13:34:52 -0300

    chore: add axios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,19 +1,29 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositiryItem";
-import api from "../api/fakeApi.json"
+import axios from "axios";
 
 import "../styles/repositories.scss";
 
-//const repository = ["unform","Forms in React","https://github/unform/unform"] Acabei fazendo uma fake api para esses dados
+// https://api.github.com/users/SamuelBalena/repos
 
 export function RepositoryList(){
+    const [ repositories, setRepositories ] = useState([])
+
+    useEffect(()=>{
+        axios.get("https://api.github.com/users/SamuelBalena/repos")
+            .then(response => {
+                setRepositories(response.data)
+            })
+    },[])
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repositoryName={api.name} repositoryDescription={api.description} repositoryLink={api.link}/>
-                <RepositoryItem repositoryName={api.name} repositoryDescription={api.description} repositoryLink={api.link}/>
-                <RepositoryItem repositoryName={api.name} repositoryDescription={api.description} repositoryLink={api.link}/>
+              {repositories.map(repository => {
+                return <RepositoryItem repository = {repository} />
+              })}
             </ul>
         </section>
     )
